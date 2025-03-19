e8433e02d839218b1ad94273d16c477a0a509194
Rucha Salunke <76227507+ruchasalunke@users.noreply.github.com>
2025-03-19 16:38:19 +0530

    Create 240 - Search a 2D Matrix II.cpp
    Write an efficient algorithm that searches for a value target in an m x n integer matrix matrix. This matrix has the following properties:
    
    Integers in each row are sorted in ascending from left to right.
    Integers in each column are sorted in ascending from top to bottom.
    
    https://leetcode.com/problems/search-a-2d-matrix-ii/

---
+++ 240 - Search a 2D Matrix II.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) 
+    {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int r = 0;
+        int c = n-1;
+        while(r<m && c>=0)
+        {
+            if(matrix[r][c] == target)
+                return true;
+            matrix[r][c] > target ? c-- : r++;    
+        }    
+        return false;
+    }
+};
