516707bacc8c264bb6f1c4b7ddb5fa760fdffb34
Rucha Salunke <76227507+ruchasalunke@users.noreply.github.com>
2025-03-21 23:58:37 +0530

    Update 07- Reverse Integer.cpp

--- 07- Reverse Integer.cpp
+++ 07- Reverse Integer.cpp
@@ -7,9 +7,7 @@ public:
             {
                 int rem = x%10;
                 if(rev < INT_MIN/10 || rev > INT_MAX/10)
-                {
                     return 0;
-                }
                 rev = rev*10 + rem;
                 x /= 10;
             }
